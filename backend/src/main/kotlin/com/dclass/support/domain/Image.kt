3c981abab891f78df86933a3dbb15eb508bbb590
Woongbae Yun <devbelly@naver.com>
2024-02-20 01:09:51 +0900

    feat : 이미지 값 객체를 구현한다

--- backend/src/main/kotlin/com/dclass/support/domain/Image.kt
+++ backend/src/main/kotlin/com/dclass/support/domain/Image.kt
@@ -1,4 +1,23 @@
 package com.dclass.support.domain
 
-class Image {
+import jakarta.persistence.Column
+import jakarta.persistence.Embeddable
+import java.net.URL
+
+@Embeddable
+class Image(
+    @Column(nullable = false)
+    val url: String
+) {
+    init {
+        validateUrl(url)
+    }
+
+    private fun validateUrl(url: String) {
+        try {
+            URL(url)
+        } catch (e: Exception) {
+            throw IllegalArgumentException("올바른 형식의 URL이 아닙니다. $url")
+        }
+    }
 }
