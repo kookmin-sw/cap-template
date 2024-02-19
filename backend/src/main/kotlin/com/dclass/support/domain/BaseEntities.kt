03d7e20a3a67d6bea5dd414e95cd32d86284c5f7
Woongbae Yun <devbelly@naver.com>
2024-02-20 01:09:08 +0900

    feat : 베이스 엔티티를 구현한다

--- backend/src/main/kotlin/com/dclass/support/domain/BaseEntities.kt
+++ backend/src/main/kotlin/com/dclass/support/domain/BaseEntities.kt
@@ -1,4 +1,20 @@
 package com.dclass.support.domain
 
-class BaseEntities {
+import jakarta.persistence.GeneratedValue
+import jakarta.persistence.GenerationType
+import jakarta.persistence.Id
+import jakarta.persistence.MappedSuperclass
+import java.util.*
+
+@MappedSuperclass
+abstract class BaseEntity(
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    val id: Long = 0L
+) {
+    override fun equals(other: Any?): Boolean =
+        Objects.equals(id, (other as? BaseEntity)?.id)
+
+    override fun hashCode(): Int =
+        Objects.hashCode(id)
 }
