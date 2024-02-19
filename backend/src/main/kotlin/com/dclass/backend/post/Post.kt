cbfbd03769e5e64f1418d4b268d2974f0dffb8f4
Woongbae Yun <devbelly@naver.com>
2024-02-20 01:10:41 +0900

    feat : Post 애그리거트를 구현한다

--- backend/src/main/kotlin/com/dclass/backend/post/Post.kt
+++ backend/src/main/kotlin/com/dclass/backend/post/Post.kt
@@ -1,4 +1,78 @@
 package com.dclass.backend.post
 
-class Post {
+import com.dclass.support.domain.BaseEntity
+import com.dclass.support.domain.Image
+import jakarta.persistence.*
+import java.time.LocalDateTime
+
+@Entity
+@Table
+class Post(
+    @Column(nullable = false)
+    val userId: Long,
+
+    title: String = "",
+
+    content: String = "",
+
+    images: List<Image> = emptyList(),
+
+    @Embedded
+    var postCount: PostCount = PostCount(),
+
+    isQuestion: Boolean = false,
+
+    @Column(nullable = false)
+    val createdDateTime: LocalDateTime = LocalDateTime.now(),
+
+    modifiedDateTime: LocalDateTime = LocalDateTime.now(),
+
+    id: Long = 0L,
+) : BaseEntity(id) {
+
+    @Column(nullable = false, length = 100)
+    var title: String = title
+        private set
+
+    @Column(nullable = false, length = 255)
+    var content: String = content
+        private set
+
+    @ElementCollection(fetch = FetchType.EAGER)
+    @CollectionTable(name = "post_images")
+    private val _images: MutableList<Image> = images.toMutableList()
+
+    val images: List<Image>
+        get() = _images
+
+    @Column(nullable = false)
+    var isQuestion: Boolean = isQuestion
+        private set
+
+    @Column(nullable = false)
+    var modifiedDateTime: LocalDateTime = modifiedDateTime
+        private set
+
+    val thumbnail: String?
+        get() = _images.firstOrNull()?.url
+
+    fun update(title: String, content: String, images: List<Image>) {
+        this.title = title
+        this.content = content
+        this._images.clear()
+        this._images.addAll(images)
+        this.modifiedDateTime = LocalDateTime.now()
+    }
+
+    fun increaseViewCount() {
+        postCount = postCount.increaseViewCount()
+    }
+
+    fun increaseReplyCount() {
+        postCount = postCount.increaseReplyCount()
+    }
+
+    fun increaseLikeCount() {
+        postCount = postCount.increaseLikeCount()
+    }
 }
--- backend/src/main/kotlin/com/dclass/backend/post/PostCount.kt
+++ backend/src/main/kotlin/com/dclass/backend/post/PostCount.kt
@@ -1,4 +1,14 @@
 package com.dclass.backend.post
 
-class PostCount {
+import jakarta.persistence.Embeddable
+
+@Embeddable
+class PostCount(
+    val viewCount: Int = 0,
+    val likeCount: Int = 0,
+    val replyCount: Int = 0
+) {
+    fun increaseViewCount() = PostCount(viewCount + 1, likeCount, replyCount)
+    fun increaseLikeCount() = PostCount(viewCount, likeCount + 1, replyCount)
+    fun increaseReplyCount() = PostCount(viewCount, likeCount, replyCount + 1)
 }
