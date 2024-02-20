b91c29d6832886457b4443c0c85db023bba367ca
Woongbae Yun <devbelly@naver.com>
2024-02-20 12:40:47 +0900

    feat : Comment 애그리거트를 구현한다

---
+++ backend/src/main/kotlin/com/dclass/backend/comment/Comment.kt
@@ -0,0 +1,43 @@
+package com.dclass.backend.comment
+
+import com.dclass.support.domain.BaseEntity
+import jakarta.persistence.Column
+import jakarta.persistence.Embedded
+import jakarta.persistence.Entity
+import org.hibernate.annotations.SQLDelete
+import org.hibernate.annotations.SQLRestriction
+
+@SQLDelete(sql = "update comment set deleted = true where id = ?")
+@SQLRestriction("deleted = false")
+@Entity
+class Comment(
+    @Column(nullable = false)
+    val userId: Long,
+
+    @Column(nullable = false)
+    val postId: Long,
+
+    content: String = "",
+
+    commentLikes: CommentLikes = CommentLikes(),
+
+    id: Long = 0L
+) : BaseEntity(id) {
+
+    @Column(nullable = false)
+    private var deleted: Boolean = false
+
+    @Column(nullable = false, length = 255)
+    var content: String = content
+        private set
+
+    @Embedded
+    var answers: CommentLikes = commentLikes
+        private set
+
+    val likeCount: Int
+        get() = answers.count
+
+    fun likedBy(userId: Long) =
+        answers.findUserById(userId)
+}
