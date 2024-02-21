9afba6ee71d04249a289c6cfe4f2b7382ae12c55
Woongbae Yun <devbelly@naver.com>
2024-02-21 09:50:42 +0900

    feat: 도메인 이벤트 발행을 위해 BaseRootEntity를 추가한다

--- backend/src/main/kotlin/com/dclass/support/domain/BaseEntities.kt
+++ backend/src/main/kotlin/com/dclass/support/domain/BaseEntities.kt
@@ -4,6 +4,7 @@ import jakarta.persistence.GeneratedValue
 import jakarta.persistence.GenerationType
 import jakarta.persistence.Id
 import jakarta.persistence.MappedSuperclass
+import org.springframework.data.domain.AbstractAggregateRoot
 import java.util.*
 
 @MappedSuperclass
@@ -12,6 +13,19 @@ abstract class BaseEntity(
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     val id: Long = 0L
 ) {
+    override fun equals(other: Any?): Boolean =
+        Objects.equals(id, (other as? BaseEntity)?.id)
+
+    override fun hashCode(): Int =
+        Objects.hashCode(id)
+}
+
+@MappedSuperclass
+abstract class BaseRootEntity<T : AbstractAggregateRoot<T>>(
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    val id: Long = 0L
+) : AbstractAggregateRoot<T>() {
     override fun equals(other: Any?): Boolean =
         Objects.equals(id, (other as? BaseEntity)?.id)
 
