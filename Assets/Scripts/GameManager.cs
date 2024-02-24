e90b72dd1d4c6b07e708ac726e6196a84d1e8843
dydwo0809 <dydwo0809@naver.com>
2024-02-25 01:04:17 +0900

    포톤 네트워크 추가 및 게임 메니저 싱글톤 적용
    게임 메니저 싱글톤 패턴 사용하도록 변경
    포톤 네트워크를 이용한 멀티플레이 추가

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -4,15 +4,41 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    private static GameManager _instance;
+    public static GameManager Instance
+    {
+        get
+        {
+            if (!_instance)
+            {
+                _instance = FindObjectOfType<GameManager>();
+                if (!_instance)
+                {
+                    GameObject obj = new GameObject();
+                    obj.name = "GameManager";
+                    _instance = obj.AddComponent(typeof(GameManager)) as GameManager;
+                }
+            }
+            return _instance;
+        }
+    }
+
+    public PhotonManager photonManager;
+
+    void Awake()
+    {
+        photonManager = GameObject.Find("PhotonManager").GetComponent<PhotonManager>();
+    }
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
