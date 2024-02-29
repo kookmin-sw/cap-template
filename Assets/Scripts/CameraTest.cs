bc5e71867b324294403c3940bfe646937d228568
dydwo0809 <dydwo0809@naver.com>
2024-02-29 22:10:16 +0900

    각종 버그 수정
    다른 캐릭터 움직이던 문제 수정
    카메라 이동 수정

--- Assets/Scripts/CameraTest.cs
+++ Assets/Scripts/CameraTest.cs
@@ -11,13 +11,23 @@ public class CameraTest : MonoBehaviour
 
     public Vector3 offset;
 
+    Vector3 newPosition;
+
     // Update is called once per frame
-    void Update()
+    void Start()
     {
+        this.gameObject.transform.rotation = Quaternion.Euler(0, 0, 0);
     }
     private void LateUpdate()
     {
-        Vector3 newPosition = player.gameObject.transform.position + offset;
+        if (player != null)
+        {
+            newPosition = player.gameObject.transform.position + offset;
+        }
+        else
+        {
+            newPosition = new Vector3(0f, 0.2f, 0f) + offset;
+        }
         this.gameObject.transform.position = newPosition;
     }
 }
