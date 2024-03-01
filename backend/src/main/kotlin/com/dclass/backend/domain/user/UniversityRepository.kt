7aa55498a10469be9c90b9a0a677fb4d29360c1d
Woongbae Yun <devbelly@naver.com>
2024-03-01 16:26:29 +0900

    feat : University 객체를 찾아주는 메서드를 구현한다

--- backend/src/main/kotlin/com/dclass/backend/domain/user/UniversityRepository.kt
+++ backend/src/main/kotlin/com/dclass/backend/domain/user/UniversityRepository.kt
@@ -5,4 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository
 interface UniversityRepository: JpaRepository<University, Long> {
     fun findByName(name: String): University
     fun existsByEmailSuffix(emailSuffix: String): Boolean
+    fun findByEmailSuffix(emailSuffix: String): University
 }
