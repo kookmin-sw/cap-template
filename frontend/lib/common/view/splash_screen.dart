322f97ca5cf6df3dc7b80a228e587f07db3d56d9
jia5232 <jia5232@naver.com>
2024-03-05 11:31:32 +0900

    feat: splash screen 추가

---
+++ frontend/lib/common/view/splash_screen.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:frontend/common/const/colors.dart';
+import 'package:frontend/common/layout/default_layout.dart';
+
+class SplashScreen extends StatelessWidget {
+  const SplashScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return DefaultLayout(
+      backgroundColor: PRIMARY_COLOR.withOpacity(0.8),
+      child: SizedBox(
+        width: MediaQuery.of(context).size.width,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Image.asset(
+              'asset/imgs/decle.png',
+              width: MediaQuery.of(context).size.width / 2,
+            ),
+            const SizedBox(
+              height: 16.0,
+            ),
+            CircularProgressIndicator(
+              color: Colors.white,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
