72332a050545d411dd538599240e46d607afb0d0
Woongbae Yun <devbelly@naver.com>
2024-03-06 13:52:21 +0900

    feat : Comment 생성 및 수정시간을 추가한다

--- backend/src/main/kotlin/com/dclass/backend/domain/comment/Comment.kt
+++ backend/src/main/kotlin/com/dclass/backend/domain/comment/Comment.kt
@@ -6,6 +6,7 @@ import jakarta.persistence.Embedded
 import jakarta.persistence.Entity
 import org.hibernate.annotations.SQLDelete
 import org.hibernate.annotations.SQLRestriction
+import java.time.LocalDateTime
 
 @SQLDelete(sql = "update comment set deleted = true where id = ?")
 @SQLRestriction("deleted = false")
@@ -21,14 +22,24 @@ class Comment(
 
     commentLikes: CommentLikes = CommentLikes(),
 
-    id: Long = 0L
-) : BaseEntity(id) {
+    id: Long = 0L,
+
+    @Column(nullable = false)
+    val createdDateTime: LocalDateTime = LocalDateTime.now(),
+
+    modifiedDateTime: LocalDateTime = LocalDateTime.now(),
+
+    ) : BaseEntity(id) {
 
     @Column(nullable = false)
     private var deleted: Boolean = false
 
     @Column(nullable = false, length = 255)
     var content: String = content
+        private set
+
+    @Column(nullable = false)
+    var modifiedDateTime: LocalDateTime = modifiedDateTime
         private set
 
     @Embedded
