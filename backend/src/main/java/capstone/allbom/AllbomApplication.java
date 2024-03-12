e5c59b11cda80c7d5e98348f3829e1f0a5e8aac9
Eun-sun-Lee <eunsun2080@kookmin.ac.kr>
2024-03-12 15:01:07 +0900

    chore : 백엔드 초기 환경설정

---
+++ backend/src/main/java/capstone/allbom/AllbomApplication.java
@@ -0,0 +1,13 @@
+package capstone.allbom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AllbomApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AllbomApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/capstone/allbom/AllbomApplicationTests.java
@@ -0,0 +1,13 @@
+package capstone.allbom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AllbomApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
