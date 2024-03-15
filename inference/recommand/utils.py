598c1a468c818c6b8f4760af1c1ff3820de20f30
seungwoo1124 <seungwoo1124@kookmin.ac.kr>
2024-03-15 15:26:05 +0900

    modify environment variable name because reserved in aws

--- inference/recommand/utils.py
+++ inference/recommand/utils.py
@@ -8,8 +8,8 @@ import os
 from io import BytesIO
 from itertools import chain
 
-aws_access_key_id = os.getenv('AWS_ACCESS_KEY')
-aws_secret_access_key = os.getenv('AWS_SECRET_ACCESS_KEY')
+aws_access_key_id = os.getenv('RECOMMEND_ACCESS_KEY')
+aws_secret_access_key = os.getenv('RECOMMEND_SECRET_ACCESS_KEY')
 
 session = boto3.session.Session(
     aws_access_key_id=aws_access_key_id,
