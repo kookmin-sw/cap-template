c4dedc9134a069cd1bde51114386e3e1f92e1d71
youjin43 <wlwlfdl3324@naver.com>
2024-03-20 19:00:57 +0900

    door 수정ㅇ중

--- Assets/Scripts/Interact/Object/Door.cs
+++ Assets/Scripts/Interact/Object/Door.cs
@@ -5,9 +5,10 @@ using UnityEngine;
 public class Door : MonoBehaviour
 {
     public bool open = false;
+    public float smoot = 2f;
+
     public float doorOpenAngle = 90f;
     public float doorCloseAngle = 0f;
-    public float smoot = 2f;
 
     public Vector3 doorOpenVector = new Vector3(0, 90f, 0);
     public Vector3 doorCloseVector = new Vector3(0f, 0f,0f);
@@ -26,15 +27,19 @@ public class Door : MonoBehaviour
 
         if (open)
         {
-            Quaternion targetRotation = Quaternion.Euler(current_vector+doorOpenAngle);
-            transform.localRotation = Quaternion.Slerp(transform.rotation, targetRotation, smoot * Time.deltaTime);
+            //transform.rotation = Quaternion.Euler(current_vector+doorOpenVector);
+            transform.rotation = Quaternion.Slerp(transform.rotation,Quaternion.Euler(), smoot * Time.deltaTime)
+
+            //Quaternion targetRotation = Quaternion.Euler(current_vector+doorOpenAngle);
+            //transform.localRotation = Quaternion.Slerp(transform.rotation, targetRotation, smoot * Time.deltaTime);
 
             //transform.localRotation = Quaternion.Slerp(transform.localRotation, Quaternion.Euler(current + doorOpenAngle), smoot * Time.deltaTime);
         }
         else
         {
-            Quaternion targetRotation2 = Quaternion.Euler(doorCloseAngle);
-            transform.localRotation = Quaternion.Slerp(current_angle, targetRotation2, smoot * Time.deltaTime);
+            transform.rotation = Quaternion.Euler(current_vector + doorCloseVector);
+            //Quaternion targetRotation2 = Quaternion.Euler(doorCloseAngle);
+            //transform.localRotation = Quaternion.Slerp(current_angle, targetRotation2, smoot * Time.deltaTime);
 
             //transform.localRotation = Quaternion.Slerp(transform.localRotation, Quaternion.Euler(current + doorCloseAngle), smoot * Time.deltaTime);
         }
