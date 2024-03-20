d58274dd35bae2c0c3c1152451dd7c68f4dfb58a
jhM00n <answlgns2056@kookmin.ac.kr>
2024-03-20 14:49:39 +0900

    add terraform binary install script & gitignore

---
+++ inference/lambda/lambda_dockerfile_with_terraform/install_terraform_binary.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# terraform1.7.5, arm64
+wget https://releases.hashicorp.com/terraform/1.7.5/terraform_1.7.5_linux_arm64.zip
+# dockerfile image build에 사용한 binary는 arm64임.
+
+# terraform1.7.5, amd64
+wget https://releases.hashicorp.com/terraform/1.7.5/terraform_1.7.5_linux_amd64.zip
