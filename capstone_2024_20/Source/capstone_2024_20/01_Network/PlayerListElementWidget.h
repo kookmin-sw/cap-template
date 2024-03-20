efe83322ad13060a0f33b56c0f230e053ecc18de
hyunseo <hansx2079@naver.com>
2024-03-20 22:30:07 +0900

    PlayerListWidget 추가

---
+++ capstone_2024_20/Source/capstone_2024_20/01_Network/PlayerListElementWidget.h
@@ -0,0 +1,26 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "PlayerListElementWidget.generated.h"
+
+/**
+ * 
+ */
+class UTextBlock;
+UCLASS()
+class CAPSTONE_2024_20_API UPlayerListElementWidget : public UUserWidget
+{
+	GENERATED_BODY()
+
+public:
+	UPROPERTY(meta=(BindWidget))
+	UTextBlock* Number;
+	UPROPERTY(meta=(BindWidget))
+	UTextBlock* Name;
+
+	void SetInfo(const FString& NewNumber, const FString& NewName);
+	
+};
