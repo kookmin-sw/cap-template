00457e3e84e0110161d5af26abf4c6326c1d98d7
jhM00n <answlgns2056@kookmin.ac.kr>
2024-03-20 15:02:29 +0900

    modify install terraform binary & dockerfile(remove requirements.txt)

--- inference/lambda/lambda_dockerfile_with_terraform/install_terraform_binary.sh
+++ inference/lambda/lambda_dockerfile_with_terraform/install_terraform_binary.sh
@@ -5,4 +5,4 @@ wget https://releases.hashicorp.com/terraform/1.7.5/terraform_1.7.5_linux_arm64.
 # dockerfile image build에 사용한 binary는 arm64임.
 
 # terraform1.7.5, amd64
-wget https://releases.hashicorp.com/terraform/1.7.5/terraform_1.7.5_linux_amd64.zip
+# wget https://releases.hashicorp.com/terraform/1.7.5/terraform_1.7.5_linux_amd64.zip
