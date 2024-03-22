daa608f9e6600c6dff0eb6f7fd14cf316d67de7c
youjin43 <wlwlfdl3324@naver.com>
2024-03-22 11:10:58 +0900

    이슈 35 문 제대로 된 축으로 돌아가게 했습니다!!!!
    이제 세세한것만 수정하면 됩니다ㅠㅠㅠㅠㅠㅜㅠㅜㅠㅠ

--- Assets/Scripts/Interact/Object/Door.cs
+++ Assets/Scripts/Interact/Object/Door.cs
@@ -7,41 +7,34 @@ public class Door : MonoBehaviour
     public bool open = false;
     public float smoot = 2f;
 
-    public float doorOpenAngle = 90f;
-    public float doorCloseAngle = 0f;
+    public Vector3 doorOpenVector = new Vector3(0, -90f, 0);
 
-    public Vector3 doorOpenVector = new Vector3(0, 90f, 0);
-    public Vector3 doorCloseVector = new Vector3(0f, 0f,0f);
+    Quaternion ToDoorAngle;
+
+    private void Update()
+    {
+        transform.rotation = Quaternion.Slerp(transform.rotation, ToDoorAngle, smoot * Time.deltaTime);
+    }
 
     public void ChangeDoorState()
     {
         open = !open;
+        ChangeDoorAngle(open);
     }
 
-
-
-    void Update()
+    void ChangeDoorAngle(bool open)
     {
-        Quaternion current_angle = Quaternion.Euler(transform.eulerAngles);
-        Vector3 current_vector = transform.eulerAngles;
-
         if (open)
         {
-            //transform.rotation = Quaternion.Euler(current_vector+doorOpenVector);
-            //transform.rotation = Quaternion.Slerp(transform.rotation,Quaternion.Euler(), smoot * Time.deltaTime)
+            ToDoorAngle = Quaternion.Euler(transform.eulerAngles + doorOpenVector);
 
-            //Quaternion targetRotation = Quaternion.Euler(current_vector+doorOpenAngle);
-            //transform.localRotation = Quaternion.Slerp(transform.rotation, targetRotation, smoot * Time.deltaTime);
-
-            //transform.localRotation = Quaternion.Slerp(transform.localRotation, Quaternion.Euler(current + doorOpenAngle), smoot * Time.deltaTime);
         }
         else
         {
-            //transform.rotation = Quaternion.Euler(current_vector + doorCloseVector);
-            //Quaternion targetRotation2 = Quaternion.Euler(doorCloseAngle);
-            //transform.localRotation = Quaternion.Slerp(current_angle, targetRotation2, smoot * Time.deltaTime);
-
-            //transform.localRotation = Quaternion.Slerp(transform.localRotation, Quaternion.Euler(current + doorCloseAngle), smoot * Time.deltaTime);
+            ToDoorAngle = Quaternion.Euler(transform.eulerAngles - doorOpenVector);
         }
+
     }
+
 }
+
