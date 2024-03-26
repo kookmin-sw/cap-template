fba05b90dc060d87b09f6b7e986d1428ec7fb8ab
QueueMin <okkimok123@kookmin.ac.kr>
2024-03-26 23:18:35 +0900

    add: kuberay init

--- basic_kubernetes_cluster/main.tf
+++ basic_kubernetes_cluster/main.tf
@@ -245,3 +245,35 @@ resource "helm_release" "kube-prometheus-stack" {
   
   depends_on = [ module.eks ]
 }
+
+resource "helm_release" "kuberay_operator" {
+  name       = "kuberay-operator"
+  chart      = "kuberay-operator"
+  version    = "1.1.0"
+  repository = "https://ray-project.github.io/kuberay-helm/"
+  namespace  = "kuberay"
+  create_namespace = true
+
+  depends_on = [module.eks]
+}
+
+resource "helm_release" "raycluster" {
+  name       = "raycluster"
+  chart      = "ray-cluster"
+  version    = "1.1.0"
+  repository = "https://ray-project.github.io/kuberay-helm/"
+  namespace  = "kuberay"
+  create_namespace = true
+
+  depends_on = [resource.helm_release.kuberay_operator]
+
+  set {
+    name  = "service.type"
+    value = "LoadBalancer"
+  }
+
+  set {
+    name  = "head.headService.metadata.annotations.service\\.beta\\.kubernetes\\.io/aws-load-balancer-scheme"
+    value = "internet-facing"
+  }
+}
