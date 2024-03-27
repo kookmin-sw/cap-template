86ff94e0978091b74120cd42f043492f3b319ba5
Autumn Na <autumn.na.dev@gmail.com>
2024-03-27 20:37:48 +0900

    CSS-39 [프로그램][신규] 스테이지_이벤트_스폰 이벤트 스폰 임시 로직 추가

--- capstone_2024_20/Source/capstone_2024_20/Sailing/SailingSystem.h
+++ capstone_2024_20/Source/capstone_2024_20/Sailing/SailingSystem.h
@@ -6,6 +6,7 @@
 
 class AMyShip;
 class AEnemyShip;
+class AEvent;
 
 UCLASS()
 class CAPSTONE_2024_20_API ASailingSystem : public AActor
@@ -18,11 +19,16 @@ public:
 	virtual void Tick(float DeltaTime) override;
 
 	void SpawnEnemyShip();
+	void SpawnEvent();
+	
 	void SetMyShip();
 
 private:
 	inline static float SpawnEnemyShipTimer = 0.0f;
 	TArray<AEnemyShip*> EnemyShips;
+
+	inline static float SpawnEventTimer = 0.0f;
+	TArray<AEvent*> Events;
 
 	UPROPERTY()
 	AMyShip* MyShip;
